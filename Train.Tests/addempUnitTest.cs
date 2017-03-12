2b338c03c711b5b07dbc01fe66a731e73a59c1ab
Sri Darshana <sridarshana92@gmail.com>
2017-03-12 21:54:43 +0530

    Add files via upload

---
+++ Train.Tests/addempUnitTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Train.Tests
+{
+    [TestClass]
+    public class addempUnitTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void ShowSucessfulMessage()
+        {
+            b = 1;
+            Assert.
+        }
+    }
+}
